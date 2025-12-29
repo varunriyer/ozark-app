756c7d10754ebd106a647ec055995974397c4705
varunriyer <rvaruniyer@gmail.com>
2025-12-30 00:40:19 +0530

    Feat: UI displays raw file content successfully

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,25 +1,29 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Import CommonModule for directives
+import { CommonModule } from '@angular/common';
 import { FileParserService } from '../../services/file-parser.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule], // Add CommonModule here
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
+  fileName: string = '';
+  fileContent: string = '';
+
   constructor(private fileService: FileParserService) {}
 
   onFileSelected(event: any) {
     const file: File = event.target.files[0];
 
     if (file) {
-      console.log('File selected:', file.name);
-      // Pass the file to our service
-      this.fileService.parseFile(file).then((content) => {
-        console.log('File processing complete.');
+      this.fileName = file.name;
+
+      // Use our service to read the file
+      this.fileService.parseFile(file).then((text) => {
+        this.fileContent = text;
       });
     }
   }
